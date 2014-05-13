9092c507b97d46dd7335b4021d68197569ec5adc
Chris Frank <chris@thenepotist.com>
2014-05-13 13:07:52 -0400

    using memcachier credentials, not memcache

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,9 +5,9 @@ require 'gapps_openid'
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
     require 'openid/store/memcache'
-    memcached_client = Dalli::Client.new(ENV['MEMCACHE_SERVERS'],
-                                           :username => ENV['MEMCACHE_USERNAME'],
-                                           :password => ENV['MEMCACHE_PASSWORD'])
+    memcached_client = Dalli::Client.new(ENV['MEMCACHIER_SERVERS'],
+                                           :username => ENV['MEMCACHIER_USERNAME'],
+                                           :password => ENV['MEMCACHIER_PASSWORD'])
     provider :open_id, name: 'google_apps',
       identifier: 'https://www.google.com/accounts/o8/site-xrds?hd=thefutureproject.org',
       store: OpenID::Store::Memcache.new(memcached_client)
