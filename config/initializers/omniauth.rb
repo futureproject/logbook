44dd89894d44c7b623ec42a455fff03c9c3ac23f
Chris Frank <chris@thenepotist.com>
2014-06-14 12:33:16 -0400

    oauth dance

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,9 +6,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.staging? || Rails.env.production? || ENV['OPENID_STORE'] == 'memcache'
     # Locally, these env vars will be blank, and it will connect to the local memcached
     # client running on the standard port
-    memcached_client = Dalli::Client.new(ENV['MEMCACHE_SERVERS'],
-                                         :username => ENV['MEMCACHE_USERNAME'],
-                                         :password => ENV['MEMCACHE_PASSWORD'])
+    memcached_client = Dalli::Client.new(ENV['MEMCACHIER_SERVERS'],
+                                         :username => ENV['MEMCACHIER_USERNAME'],
+                                         :password => ENV['MEMCACHIER_PASSWORD'])
     provider :google_apps, domain: 'thefutureproject.org',
                            store: OpenID::Store::Memcache.new(memcached_client)
   else
