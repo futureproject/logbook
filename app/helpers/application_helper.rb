b17c188a69b3d1a25a42077f6e0c5f2f56136850
Chris Frank <chris@thenepotist.com>
2014-07-06 12:48:16 -0400

    roots to something usable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,12 @@
 module ApplicationHelper
-  def nav_link(link_text, link_path)
+  def nav_link_to(link_text, link_path)
     class_name = current_page?(link_path) ? 'current' : ''
     content_tag(:li, class: class_name) do
       link_to link_text, link_path
     end
+  end
+
+  def page_title
+    "DreamOS"
   end
 end
