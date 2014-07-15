44a0607a1c8fd249fc1a305c7757692f83bec928
Chris Frank <chris@thenepotist.com>
2014-07-15 19:48:23 -0400

    one-on-ones test passes

--- spec/features/dds_logging_one_on_ones_spec.rb
+++ spec/features/dds_logging_one_on_ones_spec.rb
@@ -5,7 +5,25 @@ feature 'Logging one-on-ones' do
   end
 
   scenario 'from a student profile' do
+    visit people_path
+    select_a_student
+    fill_in_form
+    should_see_one_on_one
   end
+
+  def select_a_student
+    first('a.list-item').click
+  end
+
+  def fill_in_form
+    fill_in 'one_on_one[note]', with: 'it was a good session.'
+    click_button 'Log It'
+  end
+
+  def should_see_one_on_one
+    expect(page).to have_content 'it was a good session.'
+  end
+
   scenario 'and editing them'
 
 end
