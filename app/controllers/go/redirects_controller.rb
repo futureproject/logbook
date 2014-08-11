7c0e2bb0904fbb1933e33cb8a98232a1f8b3a20c
Chris Frank <chris@thenepotist.com>
2014-08-11 13:26:09 -0400

    skipping auth on redirects

--- app/controllers/go/redirects_controller.rb
+++ app/controllers/go/redirects_controller.rb
@@ -1,5 +1,6 @@
 class Go::RedirectsController < ApplicationController
   before_action :set_go_redirect, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate!, only: [:show]
 
   # GET /go/redirects
   # GET /go/redirects.json
@@ -10,6 +11,10 @@ class Go::RedirectsController < ApplicationController
   # GET /go/redirects/1
   # GET /go/redirects/1.json
   def show
+    hit_count = @go_redirect.hit_count || 0
+    hit_count += 1
+    @go_redirect.update_attributes hit_count: hit_count
+    redirect_to @go_redirect.url
   end
 
   # GET /go/redirects/new
@@ -64,7 +69,7 @@ class Go::RedirectsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_go_redirect
-      @go_redirect = Go::Redirect.find(params[:id])
+      @go_redirect = Go::Redirect.find_by(shortcut: params[:shortcut])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
