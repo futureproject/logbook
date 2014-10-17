e4f3ca9ee4fa024bd1e548f97d844e9c745b5b96
Chris Frank <chris@thenepotist.com>
2014-10-17 13:57:28 -0400

    processes AMR audio files correctly

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,3 +2,8 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+
+# Allow ".amr" as an extension for files with the mime type "application/octet-stream".
+ios_audio = MIME::Types["application/octet-stream"].first
+ios_audio.extensions << "amr"
+MIME::Types.index_extensions ios_audio
