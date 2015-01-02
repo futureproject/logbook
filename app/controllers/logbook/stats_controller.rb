fbe4da6559d8dfe57007697eec85248fe4faef56
Chris Frank <chris@thenepotist.com>
2015-01-01 19:16:03 -0500

    woo!

--- app/controllers/logbook/stats_controller.rb
+++ app/controllers/logbook/stats_controller.rb
@@ -2,14 +2,22 @@ class Logbook::StatsController < Logbook::ApplicationController
 
   # GET /logbook
   def index
-    ppl = current_scope.people.group_by_week('people.created_at').count.to_a
-    total = 0
-    ppl.each do |p|
-      total += p[1]
-      p[1] = total
-    end
-    @people = ppl
-    @projects = current_scope.projects.group_by_month('projects.created_at').count
+    @people = totalize(current_scope.people.group_by_week('people.created_at').count.to_a)
+    @projects = totalize(current_scope.projects.group_by_week('projects.created_at').count.to_a)
+    @people_leading_projects = totalize(current_scope.people.joins(:project_leaders).group_by_week('people.created_at').uniq.count.to_a)
+    @people_supporting_projects = totalize(current_scope.people.joins(:project_participants).group_by_week('people.created_at').uniq.count.to_a)
+    @engagements = totalize(current_scope.engagements.group_by_week('engagements.date').count.to_a)
   end
+
+  private
+
+    def totalize(array)
+      total = 0
+      array.each do |p|
+        total += p[1]
+        p[1] = total
+      end
+      array
+    end
 
 end
---
+++ config/initializers/chartckick.rb
@@ -0,0 +1,12 @@
+Chartkick.options = {
+  colors: ['#42c6ee'],
+  library: {
+    chart: {
+      backgroundColor: "#fdfcfd"
+    },
+    tooltip: {
+      pointFormat: '{series.name}: <b>{point.y}</b>',
+      xDateFormat: '%B %d'
+    }
+  }
+}
