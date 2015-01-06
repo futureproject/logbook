cd8a34ce35164f8cdd16247110de9edc9118f8a9
Chris Frank <chris@thenepotist.com>
2015-01-06 17:14:04 -0500

    better indicators of current table sort

--- app/helpers/logbook/application_helper.rb
+++ app/helpers/logbook/application_helper.rb
@@ -8,4 +8,17 @@ module Logbook::ApplicationHelper
     url_for(scope_params.merge(args))
   end
 
+  def table_link_to name, filter
+    filter_is_current = true
+    filter.each do |k,v|
+      filter_is_current = false if params[k] != v
+    end
+    link_to_unless filter_is_current, name, scoped_url_for(filter) do
+      content_tag(:span, class: 'active') do
+        name
+      end
+    end
+  end
+
+
 end
