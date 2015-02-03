c2df56423339c4aba2d905641dbd4983e3f3eaa5
Chris Frank <chris@thenepotist.com>
2015-02-03 17:07:32 -0500

    filter params actually work

--- app/helpers/logbook/application_helper.rb
+++ app/helpers/logbook/application_helper.rb
@@ -1,11 +1,11 @@
 module Logbook::ApplicationHelper
 
   def scope_params
-    params.slice :scope_type, :scope_id
+    params.slice :scope_type, :scope_id, :in_group, :in_grade
   end
 
   def scoped_url_for(args)
-    url_for(params.merge(args))
+    url_for(scope_params.merge(args))
   end
 
   def table_link_to name, filter
@@ -22,7 +22,7 @@ module Logbook::ApplicationHelper
 
   def filter_link_to name, filter
     filter_is_current = params[filter.first[0]] == filter.first[1] rescue false
-    link_to_unless filter_is_current, name, scoped_url_for(filter) do
+    link_to_unless filter_is_current, name, url_for(params.merge(filter)) do
       content_tag(:span, class: 'active') do
         name
       end
