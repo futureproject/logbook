db28df0689f3c0a14679d7bd40c754a479789d70
Chris Frank <chris@thenepotist.com>
2015-02-04 13:46:13 -0500

    further caching of graphs

--- app/controllers/logbook/stats_controller.rb
+++ app/controllers/logbook/stats_controller.rb
@@ -4,9 +4,9 @@ class Logbook::StatsController < Logbook::ApplicationController
   # GET /logbook
   def index
     @people = people_over_time
-    @people_leading_projects = current_scope.people.joins(:project_leaders).group_by_week('people.created_at', range: START_DAY..Date.today, format: '%b %d').uniq.count
-    @people_supporting_projects = current_scope.people.joins(:project_participants).group_by_week('people.created_at', range: START_DAY..Date.today, format: '%b %d').uniq.count
-    @engagements = current_scope.engagements.where('date > ?', Date.new(2014,10,1)).where('date < ?', Date.today)
+    @people_leading_projects = people_leading_projects
+    @people_supporting_projects = people_supporting_projects
+    @engagements = cached_engagements
     @coaching_sessions = @engagements.where(kind: 'Coaching Session').group_by_week('engagements.date', range: START_DAY..Date.today).count
     @events = @engagements.where(kind: 'Event').group_by_week('engagements.date', range: START_DAY..Date.today).count
     @meetings = @engagements.where(kind: 'Meeting').group_by_week('engagements.date', range: START_DAY..Date.today).count
@@ -17,24 +17,43 @@ class Logbook::StatsController < Logbook::ApplicationController
 
   private
     def people_over_time
-      current_day = START_DAY
-      end_day = Date.today
-      active_ppl = []
-      inactive_ppl = []
-      new_ppl = []
-      while current_day <= end_day
-        date = current_day.strftime "%b %d"
-        active_count = current_scope.people.created_before(current_day).joins(:engagements).merge(Engagement.week_of(current_day)).uniq.count
-        inactive_count = current_scope.people.created_before(current_day).count - active_count
-        new_count = current_scope.people.week_of(current_day).joins(:engagements).merge(Engagement.week_of(current_day)).uniq.count
-        new_ppl.push [date, new_count]
-        active_ppl.push [date, active_count - new_count]
-        inactive_ppl.push [date, inactive_count]
-        current_day += 1.week
+      Rails.cache.fetch([current_scope, 'logbook/people_over_time'], expires_in: 1.day) do
+        current_day = START_DAY
+        end_day = Date.today
+        active_ppl = []
+        inactive_ppl = []
+        new_ppl = []
+        while current_day <= end_day
+          date = current_day.strftime "%b %d"
+          active_count = current_scope.people.created_before(current_day).joins(:engagements).merge(Engagement.week_of(current_day)).uniq.count
+          inactive_count = current_scope.people.created_before(current_day).count - active_count
+          new_count = current_scope.people.week_of(current_day).joins(:engagements).merge(Engagement.week_of(current_day)).uniq.count
+          new_ppl.push [date, new_count]
+          active_ppl.push [date, active_count - new_count]
+          inactive_ppl.push [date, inactive_count]
+          current_day += 1.week
+        end
+        array = [ { name: 'Newly Engaged', data: new_ppl }, { name: 'Re-Engaged', data: active_ppl }, {name: 'Inactive', data: inactive_ppl } ]
+        array
       end
-      array = [ { name: 'Newly Engaged', data: new_ppl }, { name: 'Re-Engaged', data: active_ppl }, {name: 'Inactive', data: inactive_ppl } ]
-      array
     end
 
+    def people_leading_projects
+      Rails.cache.fetch([current_scope, 'logbook/people_leading_projects'], expires_in: 1.day) do
+        current_scope.people.joins(:project_leaders).group_by_week('people.created_at', range: START_DAY..Date.today, format: '%b %d').uniq.count
+      end
+    end
+
+    def people_supporting_projects
+      Rails.cache.fetch([current_scope, 'logbook/people_supporting_projects'], expires_in: 1.day) do
+        current_scope.people.joins(:project_participants).group_by_week('people.created_at', range: START_DAY..Date.today, format: '%b %d').uniq.count
+      end
+    end
+
+    def cached_engagements
+      Rails.cache.fetch([current_scope, 'logbook/cached_engagements'], expires_in: 1.day) do
+        current_scope.engagements.where('date > ?', Date.new(2014,10,1)).where('date < ?', Date.today)
+      end
+    end
 
 end
