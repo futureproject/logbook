5c07d0d2fb1e0306aeabe7cff8db10e07b31219b
Chris Frank <chris@thenepotist.com>
2015-02-08 16:39:22 -0500

    Notes now touch their notable

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,4 @@
 class Note < ActiveRecord::Base
-  belongs_to :notable, polymorphic: true
+  belongs_to :notable, polymorphic: true, touch: true
   validates_presence_of :content, :notable_id, :notable_type
 end
