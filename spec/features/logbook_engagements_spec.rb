cd59032192ff930823b90c0b0683e273d8a273d1
Chris Frank <chris@thenepotist.com>
2015-02-22 14:15:32 -0500

    wrote a test for it

--- spec/features/logbook_engagements_spec.rb
+++ spec/features/logbook_engagements_spec.rb
@@ -30,12 +30,11 @@ feature 'Logbook engagements' do
 
   scenario 'DESTROY', js: true do
     visit logbook_engagements_path
-    selector = "#engagement_#{Engagement.last.id}"
-    within(selector) do
-      find('a[data-method=delete]').click
-    end
+    count = all('.engagement').count
+    first('a[data-method=delete]').click
     page.driver.accept_js_confirms!
-    expect(page).not_to have_selector selector
+    sleep 1
+    expect(all('.engagement').count).to eq (count - 1)
   end
 
   scenario 'duplicating' do
@@ -50,6 +49,19 @@ feature 'Logbook engagements' do
     click_link 'Edit', match: :first
     click_link 'Toggle Dream Team'
     expect(page).to have_content 'Dream Team toggled'
+  end
+
+  scenario 'adding a new person while taking attendance at an event with no school', js: true do
+    uuid = SecureRandom.uuid
+    visit logbook_engagements_path
+    first('#global-add a').click
+    select 'Gotham City', from: 'engagement[school_id]'
+    find('.selectize-input input[type=text]').set "Jimmy Smith_#{uuid}"
+    first('div[data-selectable].create').click
+    fill_in 'engagement[name]', with: 'Clasroom Visit'
+    click_button 'Create Engagement'
+    click_link "Check it out"
+    expect(page).to have_content "Jimmy Smith"
   end
 
 end
