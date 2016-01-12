3b5b3416b992f03bd4414ff1272dc3b9db8f4910
Chris Frank <chris@thenepotist.com>
2016-01-12 13:31:09 -0600

    slightly simplified auth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -8,8 +8,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   def google_options
     {
-      scope: 'https://www.googleapis.com/auth/userinfo.profile, https://www.googleapis.com/auth/userinfo.email',
-      prompt: 'select_account',
       access_type: 'offline',
       hd: 'thefutureproject.org'
     }
